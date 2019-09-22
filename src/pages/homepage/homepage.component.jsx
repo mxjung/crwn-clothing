d615df02c6af4a1369b75e2ec2afc64fd8d9fc40
Max Jung <jung.moonki94@gmail.com>
2019-09-21 17:22:54 -0700

    Broke homepage into components, finished styling menu-page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from '../../components/directory/directory.component';
+
+// Import SCSS File
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+
+export default HomePage;
